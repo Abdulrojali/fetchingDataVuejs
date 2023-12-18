ea710dfaec805d87fe755ad0f2ea80aef243f188
abdulrojali <rojaliabdul4@gmail.com>
2023-12-18 10:50:43 -0800

    commit

---
+++ fetcing/src/App.vue
@@ -0,0 +1,11 @@
+<script>
+import dataVue from './components/data.vue'
+export default{
+  components:{
+    dataVue
+  }
+}
+</script>
+<template>
+  <dataVue></dataVue>
+</template>
---
+++ fetcing/src/components/data.vue
@@ -0,0 +1,32 @@
+<script>
+import {ref} from 'vue'
+export default{
+    setup(){
+        const datas=ref(null)
+        const datafetching=async ()=>{
+            await fetch('https://jsonplaceholder.typicode.com/photos/')
+            .then(Response=>Response.json())
+            .then(data=>datas.value=data)
+            .catch(err=>console.log(err))
+        }
+        return {datas,datafetching}
+    },
+    mounted(){
+        this.datafetching()
+    }
+}
+</script>
+
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-4 d-flex flex-column gap-2 p-2" v-for="(data,index) in datas" :key="index">
+                <img :src="data.url" class="img-fluid" alt="">
+                <h3 class="h4">{{ data.title }}</h3>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+</style>
